2600e46f217f725a8f07e0069f4b9760b77c86e4
moetaz <3ezo.yasser@gmail.com>
2024-11-24 20:16:28 -0500

    edrab

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',  // Allow your Angular frontend
+    origin: '*',  // Allow your Angular frontend
     methods: ['GET', 'POST'],        // Allow necessary methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow headers
   });
